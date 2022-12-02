b6b44d5d13f8d1e5d97590f614bb38ddbc08a164
bgodfrey4 <4936@holbertonschool.com>
2022-12-02 02:40:43 +0000

    answers

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-""" Call an API """
-from os import sys
+"""module 0-gather_data_from_an_API
+Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress.
+"""
 import requests
+from sys import argv
 
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    id = int(user_id)
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(id))
-    name = user.json().get("name")
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+def get_employee_todo(id):
+    """returns information about employee TODO list progress"""
 
-    tasks = []
-    for task in todos.json():
-        if task.get("userId") == id:
-            tasks.append(task)
-    print("Employee {} is done with tasks({}/{}):".format(name,
-                                                            len(tasks), len(todos.json())))
+    data = {'Id': id}
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + 'users', params={'id': id}).json()
+    name = users[0]['name']
+    todos = requests.get(url + 'todos', params={'userId': id}).json()
+    return ([name, todos])
 
-    for task in tasks:
-        print("\t{}. {}".format(task.get("completed"), task.get("title")))
+
+def show_employee_todo(data):
+    """shows information about employee TODO list progress"""
+    name = data[0]
+    todo = data[1]
+    n = 0
+    print_string = ""
+    for task in todo:
+        if task['completed'] is True:
+            n += 1
+            print_string += '\t ' + task['title'] + '\n'
+    print("Employee {} is done with tasks({}/{}):".format(name, n, len(todo)))
+    print(print_string, end='')
+
+
+if __name__ == '__main__':
+    data = get_employee_todo(argv[1])
+    show_employee_todo(data)
